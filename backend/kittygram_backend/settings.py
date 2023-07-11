0af9d94cf0fbd0620e26a4898ff78fbd9028ab4f
Egor <ego.boiko2018@yandex.ru>
2023-07-11 18:23:15 +0000

    new1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,12 @@ from dotenv import load_dotenv
 
 
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
-SECRET_KEY = os.getenv('key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
